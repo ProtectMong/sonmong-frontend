91cb935056e7e6f29f99b909dfbff33a1a5db821
essol503 <essol503@etners.com>
2024-03-23 22:10:36 +0900

    ⚙️setting: DesignSystem Color 추가

---
+++ sonmong/Constant.swift
@@ -0,0 +1,38 @@
+//
+//  Constant.swift
+//  sonmong
+//
+//  Created by 이은솔 on 3/23/24.
+//
+
+import Foundation
+import UIKit
+
+struct Constant { }
+
+extension Constant {
+    
+    struct Color {
+        static let g1 = UIColor(red: 0.82, green: 0.82, blue: 0.82, alpha: 1.00)
+        static let g2 = UIColor(red: 0.67, green: 0.67, blue: 0.68, alpha: 1.00)
+        static let g3 = UIColor(red: 0.58, green: 0.58, blue: 0.60, alpha: 1.00)
+        static let g4 = UIColor(red: 0.43, green: 0.43, blue: 0.45, alpha: 1.00)
+        static let g5 = UIColor(red: 0.21, green: 0.21, blue: 0.22, alpha: 1.00)
+        static let g6 = UIColor(red: 0.07, green: 0.07, blue: 0.07, alpha: 1.00)
+        static let g7 = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.00)
+        
+        static let l1 = UIColor(red: 0.96, green: 0.96, blue: 0.97, alpha: 1.00)
+        static let l2 = UIColor(red: 0.90, green: 0.91, blue: 0.93, alpha: 1.00)
+        static let l3 = UIColor(red: 0.86, green: 0.87, blue: 0.88, alpha: 1.00)
+        
+        static let m1 = UIColor(red: 0.96, green: 0.64, blue: 0.74, alpha: 1.00)
+        static let m2 = UIColor(red: 0.96, green: 0.64, blue: 0.74, alpha: 1.00)
+        static let m3 = UIColor(red: 0.96, green: 0.64, blue: 0.74, alpha: 1.00)
+        static let m4 = UIColor(red: 0.96, green: 0.64, blue: 0.74, alpha: 1.00)
+        static let m5 = UIColor(red: 0.96, green: 0.64, blue: 0.74, alpha: 1.00)
+        static let m6 = UIColor(red: 0.96, green: 0.64, blue: 0.74, alpha: 1.00)
+        static let m7 = UIColor(red: 0.96, green: 0.64, blue: 0.74, alpha: 1.00)
+        static let m8 = UIColor(red: 0.96, green: 0.64, blue: 0.74, alpha: 1.00)
+    }
+    
+}
