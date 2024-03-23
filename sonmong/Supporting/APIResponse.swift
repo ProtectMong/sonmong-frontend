56f199a0119c36da64dec4fbd82da3876028cdef
essol503 <essol503@etners.com>
2024-03-23 22:08:53 +0900

    ⚙️setting: APIService 추가

---
+++ sonmong/Supporting/APIResponse.swift
@@ -0,0 +1,8 @@
+//
+//  APIResponse.swift
+//  sonmong
+//
+//  Created by 이은솔 on 3/23/24.
+//
+
+import Foundation
---
+++ sonmong/Supporting/APIService.swift
@@ -0,0 +1,8 @@
+//
+//  APIService.swift
+//  sonmong
+//
+//  Created by 이은솔 on 3/23/24.
+//
+
+import Foundation
