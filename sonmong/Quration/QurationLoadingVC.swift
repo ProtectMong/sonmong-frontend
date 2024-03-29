5674fbf7aea139d477bc2de1db85edf20964bd56
essol503 <essol503@etners.com>
2024-03-29 16:34:36 +0900

    Revert "✨feat: Quration Loading Lottie 추가"
    This reverts commit 896cc75d46801c7f8f64aea134cdb1cef7ab5756.

--- sonmong/Quration/QurationLoadingVC.swift
+++ sonmong/Quration/QurationLoadingVC.swift
@@ -20,37 +20,17 @@ class QurationLoadingVC: UIViewController, View {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         self.navigationController?.navigationBar.isHidden = true
-        self.tabBarController?.tabBar.isHidden = true
     }
     
     func bind(reactor: QurationLoadingReactor) {
         baseView.layout(superView: self.view)
         
-        reactor.state.map{ $0.isQurationFinished }
-            .distinctUntilChanged()
-            .filterNil()
-            .filter { $0 == true }
-            .asObservable()
-            .subscribe(onNext: { [weak self] _ in
-                Observable.just("test")
-                    .delay(.seconds(5), scheduler: MainScheduler.instance)
-                    .subscribe(onNext: { text in
-                        let finalVC = QurationFinalVC()
-                        let finalReactor = QurationFinalReactor()
-                        finalVC.reactor = finalReactor
-                        
-                        self?.navigationController?.pushViewController(finalVC, animated: true)
-                    })
-                    .disposed(by: self?.disposeBag ?? DisposeBag())
-            })
-            .disposed(by: disposeBag)
     }
     
 }
